63f0328c9b967fcb54f412875215ecb6c95f5e35
GNALEGA <cyrilgnale@yahoo.fr>
2019-04-12 22:19:22 +0000

    concatenate de chaine

---
+++ code.py
@@ -0,0 +1,10 @@
+def exercice1():
+	"""############################ Exercice 1 ############################################
+	#program which concatenates 2 strings and prints the resulting unified string with
+	#the EOF character at the end of it. The 2 inputstrings are asked by the program"""
+
+	print("[Exercice 1]")
+
+	str1=input('Please enter the first string value:')
+	str2=input('Please enter the second string value:')
+  print("{first}{second}".format(first=str1,second=str2)).
